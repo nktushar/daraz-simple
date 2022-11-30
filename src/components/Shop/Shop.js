30ee3a9f783c3c4aca145d7ca546c2bde3dd5dad
Nazmul Kabir <nazmulkabirtushar@gmail.com>
2022-11-30 22:57:57 +0600

    navbar added vid 4

---
+++ src/components/Shop/Shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+//import fakeData from '../../fakeData';
+
+const Shop = () => {
+    return (
+        <div>
+            <h1>This is a Shop</h1>
+        </div>
+    );
+};
+
+export default Shop;
